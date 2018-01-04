6227dce706d54a362fef27b703e88d9f9791bf00
NahisWayard <lilian.arago@outlook.fr>
2018-01-04 16:44:44 +0100

    Delta ok.

--- include/104.h
+++ include/104.h
@@ -1,14 +1,15 @@
 #ifndef __104__
 #define __104__
 
-#define XP atof(av[2])
-#define YP atof(av[3])
-#define ZP atof(av[4])
+#define PX atof(av[2])
+#define PY atof(av[3])
+#define PZ atof(av[4])
 
-#define XV atof(av[5])
-#define YV atof(av[6])
-#define ZV atof(av[7])
+#define VX atof(av[5])
+#define VY atof(av[6])
+#define VZ atof(av[7])
 
-#define PAR atof(av[8])
+#define RAD atof(av[8])
 #define ANGLE (tan((atof(av[8]) * M_PI) / 180)) * (tan((atof(av[8]) * M_PI) / 180))
+
 #endif
--- src/main.c
+++ src/main.c
@@ -12,54 +12,54 @@
 
 int check_nbr(char *str)
 {
-	for(int i = 0; str[i]; i++)
-		if ((str[i] < '0' || str[i] > '9') && ((i == 0) ? str[i] != '-' : 1))
+	for (int i = 0; str[i]; i++)
+		if (((str[i] < '0') || (str[i] > '9')) && ((i == 0) ? (str[i] != '-') : 1))
 			return (0);
 	return (1);
 }
 
-void disp(float a, float b, float c)
+void delta(float delta)
 {
-	printf("[DEBUG] Delta -> %f\n", (b * b) - (4 * a * c));
-	if ((b * b) - (4 * a * c) < 0) {
-		printf("No intersection point.\n");
-	} else if ((b * b) - (4 * a * c) == 0) {
-		printf("1 intersection points :\n");
-	} else {
-		printf("2 intersection points :\n");
-	}
+	printf("[DEBUG] DELTA: %.2f\n", delta);
 }
 
 void sphere(char **av)
 {
-	float a = (XV * XV) + (YV + YV) + (ZV + ZV);
-	float b = (2 * XP * XV) + (2 * YP * YV) + (2 * ZP * ZV);
-	float c = (XP * XP) + (YP * YP) + (ZP * ZP) - (PAR * PAR);
-	disp(a, b, c);
+	int a = VX * VX + VY * VY + VZ * VZ;
+	int b = (2 * PX * VX) + (2 * PY * VY) + (2 * PZ * VZ);
+	int c = PX * PX + PY * PY + PZ * PZ - RAD * RAD;
+
+	delta((b * b) - (4 * a * c));
 }
 
 void cylinder(char **av)
 {
-	float a = (XV * XV) + (YV * YV);
-	float b = (2 * XP * XV) + (2 * YP * YV);
-	float c = (XP * XP) + (YP * YP) - (PAR * PAR);
-	disp(a, b, c);
+	int a = VX * VX + VY * VY;
+	int b = (2 * PX * VX) + (2 * PY * VY);
+	int c = PX * PX + PY * PY - RAD * RAD;
+
+	delta((b * b) - (4 * a * c));
 }
 
 void cone(char **av)
 {
-	float a = (XV * XV) + (YV * YV) - (ZV * ZV) / ANGLE;
-	float b = (2 * XP * XV) + (2 * YP * YV) - (2 * ZP * ZV / ANGLE);
-	float c = (XP * XP) + (YP *YP) - (ZP * ZP) / ANGLE;
-	disp(a, b, c);
+	int a = VX * VX + VY * VY - ((VZ * VZ) / (tan(RAD) * tan(RAD)));
+	int b = (2 * PX * VX) + (2 * PY * VY)  - ((2 * PZ * VZ) / (tan(RAD) * tan(RAD)));
+	int c = PX * PX + PY * PY - ((PZ * PZ) / (tan(RAD) * tan(RAD)));
+
+	delta((b * b) - (4 * a * c));
 }
 
 int main(int ac, char **av)
 {
-	if (ac != 9 || av[1][1] != '\0' || !check_nbr(av[1]) || !check_nbr(av[2]) || !check_nbr(av[3]) || !check_nbr(av[4]) || !check_nbr(av[5]) || !check_nbr(av[6]) || !check_nbr(av[7]) || !check_nbr(av[8]))
-		exit (84);
-	printf((av[1][0] == 49) ? "sphere of radius %i\n" : (av[1][0] == 50) ? "cylinder of radius %i\n" : "cone of %i degree angle\n", atoi(av[8]));
-	printf("straight line going through the (%i,%i,%i) point and of direction vector (%i,%i,%i)\n", atoi(av[2]), atoi(av[3]), atoi(av[4]), atoi(av[5]), atoi(av[6]), atoi(av[7]));
+	if ((ac != 9) || (av[1][1] != '\0') || !check_nbr(av[1]) || !check_nbr(av[2]) || !check_nbr(av[3]) ||
+	    !check_nbr(av[4]) || !check_nbr(av[5]) || !check_nbr(av[6]) || !check_nbr(av[7]) || !check_nbr(av[8]))
+		exit(84);
+	printf(
+		(av[1][0] == 49) ? "sphere of radius %i\n" : (av[1][0] == 50) ? "cylinder of radius %i\n" : "cone of %i degree angle\n",
+		atoi(av[8]));
+	printf("straight line going through the (%i,%i,%i) point and of direction vector (%i,%i,%i)\n", atoi(
+		       av[2]), atoi(av[3]), atoi(av[4]), atoi(av[5]), atoi(av[6]), atoi(av[7]));
 	(av[1][0] == 49) ? sphere(av) : (av[1][0] == 50) ? cylinder(av) : cone(av);
 	return (0);
 }
